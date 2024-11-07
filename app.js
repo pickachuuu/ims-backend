98a07da6a50db223b296e36d4de4215dee8a00b9
Derek <dberb789@gmail.com>
2024-11-08 01:21:21 +0800

    feat: set up middleware and body parser, configure express and add user routes

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+const userRoutes = require('./routes/userRoutes');
+
+dotenv.config();
+
+const app = express();
+app.use(bodyParser.json());
+app.use('/api/users', userRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
