1f8960cc1b8ee110da17a1c471ed3401efa81663
Derek <dberb789@gmail.com>
2024-11-08 01:14:06 +0800

    feat: implemented userController

---
+++ controllers/userController.js
@@ -0,0 +1,52 @@
+const router = require('express').Router();
+const { User } = require('../models');
+
+const register = async (req, res) => {
+    try{
+        const {email, password, first_name, last_name} = req.body;
+        const existingUser = await User.findOne({
+            where: {
+                email: email
+            }
+        });
+
+        if (existingUser) {
+            return res.status(400).json({
+                message: 'User with this email already exists'
+            });
+        }
+
+        const newUser = await User.create({
+            email: email,
+            password: password,
+            first_name: first_name,
+            last_name: last_name
+        })
+
+        if (!newUser) {
+            return res.status(400).json({
+                message: 'Failed to create user'
+            });
+        }else{
+            return res.status(201).json({
+                message: 'User created successfully',
+                user: newUser
+            });
+        }
+
+
+    } catch (error) {
+        console.error('Error registering user:', error);
+        res.status(500).json({message: 'Internal server error'});
+    }
+};
+
+
+router.post('/register', register);
+const login = (req, res) => {
+};
+
+module.exports = {
+    register,
+    login,
+};
