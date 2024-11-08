959ca862258760982226ee64a10cf784fa13d9bf
Derek <dberb789@gmail.com>
2024-11-08 16:51:43 +0800

    feat: enable cors middleware

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 const userRoutes = require('./routes/userRoutes');
-
+const cors = require('cors');
+const corsOptions = require('./utils/corsConfig');
 dotenv.config();
 
 const app = express();
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use('/api/users', userRoutes);
 
