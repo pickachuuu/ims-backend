0a46f79f516e32493f1cce58f00dacfe68adc59e
Derek <dberb789@gmail.com>
2024-11-08 16:48:36 +0800

    feat: added login endpoint

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const Controller = require('../controllers/userController');
 
 router.post('/register', Controller.register);
+router.post('/login', Controller.login);
 
 module.exports = router;
