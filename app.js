fcafb35d8820f09f7fde6179351f83c373a72d60
Derek <dberb789@gmail.com>
2024-11-10 18:32:05 +0800

    feat: add business routes to app

--- app.js
+++ app.js
@@ -4,13 +4,14 @@ const dotenv = require('dotenv');
 const userRoutes = require('./routes/userRoutes');
 const cors = require('cors');
 const corsOptions = require('./utils/corsConfig');
+const businessRoutes = require('./routes/businessRoutes');
 dotenv.config();
 
 const app = express();
 app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use('/api/users', userRoutes);
-
+app.use('/api/business', businessRoutes);
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
