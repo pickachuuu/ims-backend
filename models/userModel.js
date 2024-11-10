4acd58a641809f3e03504044e85f57ece1c0edd9
Derek <dberb789@gmail.com>
2024-11-10 14:25:37 +0800

    refractor: added businessID to user model

--- models/userModel.js
+++ models/userModel.js
@@ -2,13 +2,21 @@ const { Model, DataTypes } = require('sequelize');
 const { hashPassword } = require('../utils/passwordUtils');
 
 module.exports = (sequelize) => {
-  class User extends Model {}
+  class User extends Model {
+    static associate(models) {
+      User.belongsTo(models.Business, {
+        foreignKey: 'businessID',
+        as: 'business'
+      });
+    }
+  }
 
   User.init({
     userID: {
       type: DataTypes.INTEGER,
       primaryKey: true,
       autoIncrement: true,
+      allowNull: false
     },
     first_name: {  
       type: DataTypes.STRING,
@@ -31,20 +39,41 @@ module.exports = (sequelize) => {
       allowNull: false,
     },
     roleID: {
-      type: DataTypes.ENUM('0', '1'),
+      type: DataTypes.INTEGER, 
       allowNull: false,
-      defaultValue: '0',
+      defaultValue: 0,
+      comment: '0: Admin, 1: User'
+    },
+    businessID: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      references: {
+        model: 'businesses',
+        key: 'businessID'
+      }
+    },
+    deletedAt: {
+      type: DataTypes.DATE,
+      allowNull: true
     }
   }, {
+    hooks: {
+      beforeCreate: async (user) => {
+        if (user.password) {
+          user.password = await hashPassword(user.password);
+        }
+      },
+      beforeUpdate: async (user) => {
+        if (user.changed('password')) {
+          user.password = await hashPassword(user.password);
+        }
+      }
+    },
     sequelize,
     modelName: 'User',
     tableName: 'users',
     timestamps: true,
-    hooks: {
-      beforeCreate: async (user) => {
-        user.password = await hashPassword(user.password);
-      }
-    }
+    paranoid: true
   });
 
   return User;
