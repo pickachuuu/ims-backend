01bbfad259a03cbd5b2f243f0ed0189ee4363345
Derek <dberb789@gmail.com>
2024-11-11 13:53:00 +0800

    feat: added product routes to app

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 const userRoutes = require('./routes/userRoutes');
-const cors = require('cors');
 const corsOptions = require('./utils/corsConfig');
 const businessRoutes = require('./routes/businessRoutes');
+const productRoutes = require('./routes/productRoutes');
+const cors = require('cors');
 dotenv.config();
 
 const app = express();
@@ -12,6 +13,7 @@ app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use('/api/users', userRoutes);
 app.use('/api/business', businessRoutes);
+app.use('/api/products', productRoutes);
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
