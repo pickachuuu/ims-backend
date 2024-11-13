b58872526a54b8de5079403b96f15ca86a28c18d
Derek <dberb789@gmail.com>
2024-11-13 23:57:49 +0800

    feat: implemented create supplier function

--- controllers/supplierController.js
+++ controllers/supplierController.js
@@ -1,7 +1,34 @@
 const { Supplier } = require('../models');
 
 const createSupplier = async (req, res) => {
-    
+    try{
+        const { supplierName, contactNo } = req.body;
+        const businessID = req.user.businessID;
+        
+        const supplier = await Supplier.create({
+            supplierName,
+            contactNo,
+            businessID
+        });
+
+        if (!supplier) {
+            return res.status(400).json({
+                message: 'Failed to create supplier'
+            });
+        } else {
+            return res.status(201).json({
+                message: 'Supplier created successfully',
+                supplier
+            });
+        }
+
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
+
 }
 
 const removeSupplier = async (req, res) => {
