c2d564e6505653e24747ba30a67462f88a47c7d0
Derek <dberb789@gmail.com>
2024-11-13 23:44:53 +0800

    feat: implemented supplier controller

---
+++ controllers/supplierController.js
@@ -0,0 +1,19 @@
+const { Supplier } = require('../models');
+
+const createSupplier = async (req, res) => {
+    
+}
+
+const removeSupplier = async (req, res) => {
+
+}
+
+const updateSupplier = async (req, res) => {
+
+}
+
+module.exports = {
+    createSupplier,
+    removeSupplier,
+    updateSupplier,
+};
