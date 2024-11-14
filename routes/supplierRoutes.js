0492fd2d1dc14ef1bb13073ed198861859450b92
Derek <dberb789@gmail.com>
2024-11-14 19:55:22 +0800

    feat: added delete supplier route

--- routes/supplierRoutes.js
+++ routes/supplierRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { createSupplier, updateSupplier } = require('../controllers/supplierController');
+const { createSupplier, updateSupplier, deleteSupplier } = require('../controllers/supplierController');
 const { protect } = require('../middlewares/authMiddleware');
 
 router.post('/create', protect, createSupplier);
 router.put('/update/:supplierID', protect, updateSupplier);
-
+router.delete('/remove/:supplierID', protect, deleteSupplier);
 module.exports = router;
