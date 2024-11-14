00edee1937dcf4f019f2dce9e86af905be0b4cdb
Derek <dberb789@gmail.com>
2024-11-14 19:28:08 +0800

    feat: added updateSupplier route

--- routes/supplierRoutes.js
+++ routes/supplierRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { createSupplier } = require('../controllers/supplierController');
+const { createSupplier, updateSupplier } = require('../controllers/supplierController');
 const { protect } = require('../middlewares/authMiddleware');
 
 router.post('/create', protect, createSupplier);
+router.put('/update/:supplierID', protect, updateSupplier);
 
 module.exports = router;
