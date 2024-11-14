656a30206ad90ff59b72b4652d00abecb79566b9
Derek <dberb789@gmail.com>
2024-11-15 00:53:37 +0800

    feat: added getProducts route

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -4,15 +4,15 @@ const { protect } = require('../middlewares/authMiddleware');
 const { 
     createProduct,
     removeProduct,
-    // getProducts, 
-    updateProduct 
+    updateProduct,
+    getProducts,
     // deleteProduct 
 } = require('../controllers/productController');
 
 router.post('/create', protect, createProduct);
 router.delete('/delete/:productID', protect, removeProduct);
 router.put('/update/:productID', protect, updateProduct);
-// router.get('/', protect, getProducts);
+router.get('/get', protect, getProducts);
 // router.delete('/:productID', protect, deleteProduct);
 
 module.exports = router;   
