2baec823d69939804d5bbf121155fec2f1fbae9a
Derek <dberb789@gmail.com>
2024-11-15 22:12:01 +0800

    feat: add category routes to app

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const corsOptions = require('./utils/corsConfig');
 const businessRoutes = require('./routes/businessRoutes');
 const productRoutes = require('./routes/productRoutes');
 const supplierRoutes = require('./routes/supplierRoutes');
+const categoryRoutes = require('./routes/categoryRoutes');
 const cors = require('cors');
 dotenv.config();
 
@@ -16,6 +17,7 @@ app.use('/api/users', userRoutes);
 app.use('/api/business', businessRoutes);
 app.use('/api/products', productRoutes);
 app.use('/api/suppliers', supplierRoutes);
+app.use('/api/categories', categoryRoutes);
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
