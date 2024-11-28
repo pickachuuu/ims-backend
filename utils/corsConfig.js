9ba86d62b41756fb6a845aa2ad4a737e29029c6e
Derek <dberb789@gmail.com>
2024-11-28 15:58:36 +0800

    docs: removed useless comments

--- utils/corsConfig.js
+++ utils/corsConfig.js
@@ -1,7 +1,7 @@
 const corsOptions = {
     origin: [
-      'http://localhost:5173',  // Vite frontend
-      'http://localhost:3000'   // Express backend
+      'http://localhost:5173', 
+      'http://localhost:3000'   
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
