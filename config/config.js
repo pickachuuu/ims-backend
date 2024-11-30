65ea2aae450ead63d7b28bccbb4f30e562eaec13
Derek <dberb789@gmail.com>
2024-11-30 12:11:38 +0800

    refactor: removed obsolete presets

--- config/config.js
+++ config/config.js
@@ -9,24 +9,6 @@ module.exports = {
     port: process.env.DB_PORT,
     dialect: "mysql",
     migrationStorageTableName: "migrations",
-  },
-  test: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: "mysql",
-    migrationStorageTableName: "migrations",
-  },
-  production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: "mysql",
-    migrationStorageTableName: "migrations",
   }
 };
 
