217eb82085f93addf8bf9ad73984bdbceffeeff9
Derek <dberb789@gmail.com>
2024-12-08 20:29:51 +0800

    feat: add route for bulk category removal in categoryRoutes

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { createCategory, deleteCategory, updateCategory, getCategories } = require('../controllers/categoryController');
+const { createCategory, deleteCategory, updateCategory, getCategories, removeCategories } = require('../controllers/categoryController');
 const { protect } = require('../middlewares/authMiddleware');
 
 router.post('/create', protect, createCategory);
 router.delete('/delete/:categoryID', protect, deleteCategory);
+router.delete('/deleteAll/', protect, removeCategories);
 router.put('/update/:categoryID', protect, updateCategory);
 router.get('/get', protect, getCategories);
 module.exports = router;
