f36b1d547e6ab10d79d1ddf22c2603fac9a254d8
Derek <dberb789@gmail.com>
2024-12-12 15:21:24 +0800

    feat: add route for bulk supplier deletion in supplierRoutes

--- routes/supplierRoutes.js
+++ routes/supplierRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { createSupplier, updateSupplier, deleteSupplier, getSuppliers } = require('../controllers/supplierController');
+const { createSupplier, updateSupplier, deleteSupplier, deleteSuppliers, getSuppliers } = require('../controllers/supplierController');
 const { protect } = require('../middlewares/authMiddleware');
 
 router.post('/create', protect, createSupplier);
 router.put('/update/:supplierID', protect, updateSupplier);
 router.delete('/remove/:supplierID', protect, deleteSupplier);
+router.delete('/removeAll/', protect, deleteSuppliers);
 router.get('/get', protect, getSuppliers);
 
 module.exports = router;
