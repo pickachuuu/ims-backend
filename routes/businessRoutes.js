229de2e68712dffffed338801e0e50a8bee20a83
Derek <dberb789@gmail.com>
2024-12-14 15:14:00 +0800

    feat: add getBusinessProfile route to businessRoutes
    - Introduced a new endpoint for retrieving business profile information.
    - Integrated the getBusinessProfile function from the businessController.
    - Ensured the route is protected with authentication middleware.

--- routes/businessRoutes.js
+++ routes/businessRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { createBusiness, editBusiness} = require('../controllers/businessController');
+const { createBusiness, editBusiness, getBusinessProfile} = require('../controllers/businessController');
 const { protect } = require('../middlewares/authMiddleware');
 
 // Protected routes
 router.post('/setup', protect, createBusiness);
 router.put('/edit', protect, editBusiness);
+router.get('/profile', protect, getBusinessProfile);
 
 module.exports = router;
